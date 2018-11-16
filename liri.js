a0ee6fd2909b3f2aa0485c24ff0c9e33d81f6ac1
Malorie Lara <malorielara@users.noreply.github.com>
2018-11-15 18:19:57 -0600

    notes of what i need to do

--- liri.js
+++ liri.js
@@ -16,3 +16,22 @@ var filename = './log.txt';
 var log = require('simple-node-logger').createSimpleFileLogger(filename);
 
 log.setLevel('all');
+
+var userCommand = process.argv[2];
+var secondCommand = process.argv[3];
+
+for (var i = 4; i < process.argv.length; i++) {
+    secondCommand += '+' + process.argv[i];
+}
+
+// to get spotify
+
+// artists names
+
+// spotify function
+    // search spotify
+
+// get tweets function
+
+// get movie function
+// OMDB
