36ba5fceee058dc3fbdce557d00714dfc912b324
Malorie Lara <malorielara@users.noreply.github.com>
2018-11-15 18:29:25 -0600

    mas

--- liri.js
+++ liri.js
@@ -32,16 +32,39 @@ var getArtistNames = function (artist) {
     return artist.name;
 };
 
-var getSpotify = function(songName) {
-    if(songName === undefined) {
+var getSpotify = function (songName) {
+    if (songName === undefined) {
         songName = "Que es mi numero favorito?";
     }
-}
+
+    spotify.search({
+            type: "track",
+            query: userCommand
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
 
 // artists names
 
 // spotify function
-    // search spotify
+// search spotify
 
 // get tweets function
 
